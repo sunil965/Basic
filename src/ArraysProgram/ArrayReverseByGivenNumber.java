e865e243230aaf6afdd9b57e0e6c491eae2b7b04
sunil956 <sunilkumar192114@gmail.com>
2023-01-20 10:02:44 +0530

    Updating ArrayReverseByGivenNumber with recursion.

--- src/ArraysProgram/ArrayReverseByGivenNumber.java
+++ src/ArraysProgram/ArrayReverseByGivenNumber.java
@@ -30,9 +30,19 @@ public class ArrayReverseByGivenNumber {
 		reverseArray(array, start + 1, end - 1);
 	}
 
+	static void reverseArrayViaRecursion(int[] arr, int start, int end) {
+		int temp;
+		while (start < end) {
+			temp = arr[start];
+			arr[start] = arr[end];
+			arr[end] = temp;
+			start++;
+			end--;
+		}
+	}
+
 	static void printArray(int[] arr) {
-		for (int i = 0; i < arr.length; i++)
-			System.out.print(arr[i] + " ");
+		for (int j : arr) System.out.print(j + " ");
 	}
 
 }
