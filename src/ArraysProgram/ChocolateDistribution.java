5e09739d9697fc15ceab743c3c4cec4dd07eab5b
sunil956 <sunilkumar192114@gmail.com>
2023-01-30 11:43:26 +0530

    Bug fixed in ChocolateDistribution Program.

--- src/ArraysProgram/ChocolateDistribution.java
+++ src/ArraysProgram/ChocolateDistribution.java
@@ -12,8 +12,6 @@ public class ChocolateDistribution {
 		if (n < m)
 			return -1;
 
-		Arrays.sort(arr);
-
 		// Largest number of chocolates
 		int min_diff = Integer.MAX_VALUE;
 		int first = 0, last = 0;
@@ -25,16 +23,19 @@ public class ChocolateDistribution {
 				last = i + m - 1;
 			}
 		}
+		System.out.println("Index of First element: "+first+ " and last element: "+last);
 		return (arr[last] - arr[first]);
 	}
 
 	public static void main(String[] args) {
-		int[] arr = { 12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 50 };
+		int[] chocolatePackages = { 12, 4, 7, 9, 2, 23, 25, 41, 30, 40, 28, 42, 30, 44, 50 };
 
 		int m = 7; // Number of students
 
-		int n = arr.length;
-		System.out.println("Minimum difference is " + findMinDiff(arr, n, m));
-
+		int n = chocolatePackages.length;
+		System.out.println("Given  array is " +Arrays.toString(chocolatePackages));
+		Arrays.sort(chocolatePackages);
+		System.out.println("Sorted array is " +Arrays.toString(chocolatePackages));
+		System.out.println("Output: Minimum difference is " + findMinDiff(chocolatePackages, n, m));
 	}
 }
