04ec63cd8872f3ee680125526fbdc62ecd478ccd
sunil956 <sunilkumar192114@gmail.com>
2023-01-31 13:14:57 +0530

    Playing in StreamExample with optional class.

--- src/java_8_features/StreamExample.java
+++ src/java_8_features/StreamExample.java
@@ -2,18 +2,20 @@ package java_8_features;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class StreamExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Student s1 = new Student("Sunil", "Kumar",001, true);
+		Student s1 = new Student("Sunil", "Kumar", 1, true);
 		Student s2 = new Student();
-		Student s3 = new Student("Anil", "Pal",002, true);
-		Student s4 = new Student("Sanjeev", "Kumar",003, true);
-		Student s5 = new Student("Ajit", "Singh",004, false);
-		Student s6 = new Student("Sachin", "Pal",005, true);
+		Student s3 = new Student("Anil", "Pal", 2, true);
+		Student s4 = new Student("Sanjeev", "Kumar", 3, true);
+		Student s5 = new Student("Ajit", "Singh", 4, false);
+		Student s6 = new Student("Sachin", "Pal", 5, true);
 		
 		List<Student> studentList = new ArrayList<>();
 		studentList.add(s1);
@@ -23,11 +25,11 @@ public class StreamExample {
 		studentList.add(s5);
 		studentList.add(s6);
 		
-		Stream<Student> stuStream = studentList.stream().filter(s-> s.fName!=null && s.isPreseent==true);
+		Stream<Student> stuStream = studentList.stream().filter(s-> s.fName!=null && s.isPreseent);
 		stuStream.forEach(s -> System.out.println("Details : "+s));
 		
 		
-		/*List<String> myList = new ArrayList<String>();
+		List<String> myList = new ArrayList<>();
 //		for(int i=0; i<100; i++) myList.add(i);
 		
 		myList.add("a");
@@ -50,25 +52,21 @@ public class StreamExample {
 		
 		Stream<String> highNumsSeq = sequentialStream.filter(p -> p !=null);
 		highNumsSeq.forEach(p -> System.out.println("High Nums sequential="+p));
-		*/
-		
-		
-		
-		
-		/*Optional<String> intOptional = highNumsSeq.reduce((i,j) -> {return i+j;});
-		if(intOptional.isPresent()) System.out.println("String is = "+intOptional.get());*/
-		
-		/*Stream<Integer> numbers = Stream.of(1,2,3,4,5);
-		Optional<Integer> intOptional = numbers.reduce((i,j) -> {return i*j;});
-		if(intOptional.isPresent()) System.out.println("Multiplication = "+intOptional.get());
-		
+
+		Optional<String> intOptional = myList.stream().filter(Objects::nonNull).reduce((i, j) -> i+j);
+		intOptional.ifPresent(s -> System.out.println("String is = " + s));
+
+		Stream<Integer> numbers = Stream.of(1,2,3,4,5);
+		Optional<Integer> intOptional1 = numbers.reduce((i,j) -> i*j);
+		intOptional1.ifPresent(integer -> System.out.println("Multiplication = " + integer));
+
 		Stream<Integer> numbers2 = Stream.of(1,2,3,4,5);
-		Optional<Integer> intOptional2 = numbers2.reduce((i,j) -> {return i+j;});
-		if(intOptional.isPresent()) System.out.println("Addition = "+intOptional2.get());
-		
+		Optional<Integer> intOptional2 = numbers2.reduce(Integer::sum);
+		intOptional2.ifPresent(integer -> System.out.println("Addition = " + integer));
+
 		Stream<Integer> numbers3 = Stream.of(1,2,3,4,5);
-		Optional<Integer> intOptional3 = numbers3.reduce((i,j) -> {return i-j;});
-		if(intOptional.isPresent()) System.out.println("Subtraction = "+intOptional3.get());*/
+		Optional<Integer> intOptional3 = numbers3.reduce((i,j) -> j-i);
+		intOptional3.ifPresent(integer -> System.out.println("Subtraction = " + integer));
 		
 //		cars.stream().filter(car -> Objects.nonNull(car)).map(car -> car.getName()).filter(carName -> Objects.nonNull(carName)).filter(carName -> carName.startsWith("M")).collect(Collectors.toList());
 //		myList.stream().filter(myList-> Object.notNull(myList));
