88873335f8d466b5a6db1157d7945617d825680e
sunil956 <sunilkumar192114@gmail.com>
2023-02-01 14:45:32 +0530

    Update RemoveDuplicate Program.

--- src/list/RemoveDuplicate.java
+++ src/list/RemoveDuplicate.java
@@ -1,80 +1,71 @@
 package list;
 
 //Java program to remove duplicates from a sorted linked list 
-class RemoveDuplicate 
-{ 
-	Node head; // head of list 
+class RemoveDuplicate {
+    Node head; // head of list
 
-	/* Linked list Node*/
-	class Node 
-	{ 
-		int data; 
-		Node next; 
-		Node(int d) {data = d; next = null; } 
-	} 
+    /* Linked list Node*/
+    static class Node {
+        int data;
+        Node next;
 
-	void removeDuplicates() 
-	{ 
-		/*Another reference to head*/
-		Node curr = head; 
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
 
-		/* Traverse list till the last node */
-		while (curr != null) { 
-			Node temp = curr; 
+    void removeDuplicates() {
+        Node curr = head;
+
+        /* Traverse list till the last node */
+        while (curr != null) {
+            Node temp = curr;
 			/*Compare current node with the next node and 
 			keep on deleting them until it matches the current 
 			node data */
-			while(temp!=null && temp.data==curr.data) { 
-				temp = temp.next;
-			} 
+            while (temp != null && temp.data == curr.data) {
+                temp = temp.next;
+            }
 			/*Set current node next to the next different 
 			element denoted by temp*/
-			curr.next = temp; 
-			curr = curr.next; 
-		} 
-	} 
-					
-	/* Utility functions */
+            curr.next = temp;
+            curr = curr.next;
+        }
+    }
 
-	/* Inserts a new Node at front of the list. */
-	public void push(int new_data) 
-	{ 
-		Node new_node = new Node(new_data); 
+    /* Inserts a new Node at front of the list. */
+    public void push(int new_data) {
+        Node newNode = new Node(new_data);
+        newNode.next = head;
+        head = newNode;
+    }
 
-		new_node.next = head; 
+    void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
 
-		head = new_node; 
-	} 
+    public static void main(String[] args) {
+        RemoveDuplicate llist = new RemoveDuplicate();
+        llist.push(20);
+        llist.push(13);
+        llist.push(13);
+        llist.push(11);
+        llist.push(11);
+        llist.push(11);
 
-	/* Function to print linked list */
-	void printList() 
-	{ 
-		Node temp = head; 
-		while (temp != null) 
-		{ 
-			System.out.print(temp.data+" "); 
-			temp = temp.next; 
-		} 
-		System.out.println(); 
-	} 
+        System.out.println("List before removal of duplicates");
+        llist.printList();
 
-	public static void main(String[] args)
-	{ 
-		RemoveDuplicate llist = new RemoveDuplicate(); 
-		llist.push(20); 
-		llist.push(13); 
-		llist.push(13); 
-		llist.push(11); 
-		llist.push(11); 
-		llist.push(11); 
-		
-		System.out.println("List before removal of duplicates"); 
-		llist.printList(); 
-		
-		llist.removeDuplicates(); 
-		
-		System.out.println("List after removal of elements"); 
-		llist.printList(); 
-	} 
+        llist.removeDuplicates();
+
+        System.out.println("List after removal of elements");
+        llist.printList();
+    }
 } 
 
