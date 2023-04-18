c357f224cc5a00c69b69b4295520683dd0714701
skumar12295 <sunilkumar192114@gmail.com>
2023-04-18 11:58:32 +0530

    Update MinimumPlatform Program.

--- src/ArraysProgram/MinimumPlatform.java
+++ src/ArraysProgram/MinimumPlatform.java
@@ -4,30 +4,30 @@ import java.util.Arrays;
 
 public class MinimumPlatform {
 
-	public static void main(String[] args) {
-		int[] arr = {900, 940, 950, 1100, 1500, 1800};
-	    int[] dep = {910, 1200, 1120, 1130, 1900, 2000};
-	    int n = arr.length; 
-	    System.out.println("Minimum Number of Platforms Required = "+ findPlatform(arr, dep, n));
-	}
+    public static void main(String[] args) {
+        int[] arr = {900, 940, 950, 1100, 1500, 1800};
+        int[] dep = {910, 1200, 1120, 1130, 1900, 2000};
+        int n = arr.length;
+        System.out.println("Minimum Number of Platforms Required = " + findPlatform(arr, dep, n));
+    }
 
-	private static int findPlatform(int[] arr, int[] dep, int n) {
-		int count=0, result=0;
-		Arrays.sort(arr);
-		Arrays.sort(dep);
-		int i=0, j=0;
-		while( i<n && j<n) {
-			if(arr[i]<dep[j]) {
-				count++;
-				i++;
-				if(count>result)
-					result=count;
-			}else {
-				count--;
-				j++;
-			}
-		}
-		return result;
-	}
+    private static int findPlatform(int[] arr, int[] dep, int n) {
+        int count = 0, result = 0;
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int i = 0, j = 0;
+        while (i < n && j < n) {
+            if (arr[i] < dep[j]) {
+                count++;
+                i++;
+                if (count > result)
+                    result = count;
+            } else {
+                count--;
+                j++;
+            }
+        }
+        return result;
+    }
 
 }
